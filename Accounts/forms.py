2164d6411a02f36877d477c49b28b885bd79488d
Barracoder <emmnauelobarany@gmail.com>
2019-05-22 15:33:11 +0300

    Added Signupform and profileupdateform
    Signed-off-by: Barracoder <emmnauelobarany@gmail.com>

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -1,44 +1,64 @@
-from django.contrib.auth.forms import ReadOnlyPasswordHashField, UserCreationForm
-from django.forms import ModelForm, PasswordInput, CharField, ValidationError
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-from .models import Participant
-
-
-class ParticipantCreationForm(UserCreationForm):
-    password1 = CharField(label='Password', widget=PasswordInput)
-    password2 = CharField(label='Password confirmation', widget=PasswordInput)
-
-    class Meta(UserCreationForm.Meta):
-        model = Participant
-        fields = ('identification', 'name', 'email', 'mobile', 'institution', 'gender', 'country', 'password', 'avatar',
-                  'date_of_birth', 'skills', 'username')
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-
-        # Save the provided password in hashed format
-        user = super(ParticipantCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
+from Info.models import Institution, Country
+from .models import Profile
 
 
-class ParticipantChangeForm(ModelForm):
-    """A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    """
-    password = ReadOnlyPasswordHashField()
+class SignUpForm(UserCreationForm):
+    GENDER = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+
+    institution = forms.ModelChoiceField(queryset=Institution.objects)
+
+    gender = forms.ChoiceField(choices=GENDER,
+                               label="",
+                               initial='',
+                               widget=forms.Select(),
+                               required=True)
+
+    country = forms.ModelChoiceField(queryset=Country.objects)
+
+    mobile = forms.CharField(max_length=50)
+    date_of_birth = forms.DateField()
 
     class Meta:
-        model = Participant
-        fields = ('identification', 'name', 'email', 'mobile', 'institution', 'gender', 'country', 'password', 'avatar',
-                  'date_of_birth', 'skills', 'username')
+        model = User
+        fields = (
+            'username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'institution', 'mobile',
+            'gender', 'country', 'date_of_birth')
+        help_texts = {
+            'username': None,
+            'email': None,
+            'password1': None,
+            'password2': None,
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        if args:
+            self.post_data = args[0]
+        else:
+            self.post_data = {}
+
+        for field_name in ('username', 'password1', 'password2'):
+            self.fields[field_name].help_text = None
+
+    def countries(self):
+        return self.fields["country"].queryset
+
+    def institutions(self):
+        return self.fields["institution"].queryset
+
+    def clean_mobile(self):
+        #  Merge country code with the mobile number
+        return int(str(self.cleaned_data.get('mobile')) + str(self.post_data.get("code", 254)))
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('institution', 'skills', 'date_of_birth', 'gender', 'country', 'mobile')
