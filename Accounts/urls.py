c3e2831333bf48d887e2249a23219defe65f4412
Barracoder <emmnauelobarany@gmail.com>
2019-05-22 15:34:55 +0300

    Added logout link
    Signed-off-by: Barracoder <emmnauelobarany@gmail.com>

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -6,6 +6,7 @@ app_name = "accounts"
 
 urlpatterns = [
     path('register', views.registration_form, name="register"),
+    path('logout', views.log_out, name="logout"),
     path('username/verify', views.verify_username, name="verify_username"),
     path('email/verify', views.verify_email, name="verify_email"),
 ]
