2563d83f2d392287b95fed32dfe42bd8ea39df9d
Barracoder <emmnauelobarany@gmail.com>
2019-05-23 14:54:28 +0300

    Added preuth, logout and profile links.
    Signed-off-by: Barracoder <emmnauelobarany@gmail.com>

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -5,8 +6,11 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
     path('register', views.registration_form, name="register"),
     path('logout', views.log_out, name="logout"),
     path('username/verify', views.verify_username, name="verify_username"),
     path('email/verify', views.verify_email, name="verify_email"),
+    path('preauth', views.pre_authenticate, name="pre_authenticate"),
+    path('profile/', views.profile, name="profile"),
 ]
