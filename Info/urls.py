cd1b221dcd7512c22d722b96f7eead7a282de49c
Barracoder <emmnauelobarany@gmail.com>
2019-06-07 20:34:18 +0300

    Added new url routes.
    Signed-off-by: Barracoder <emmnauelobarany@gmail.com>

--- Info/urls.py
+++ Info/urls.py
@@ -6,4 +6,7 @@ app_name = "info"
 
 urlpatterns = [
     path('home', views.home, name="home"),
+    path('about', views.about, name="about"),
+    path('faq', views.faq, name="faq"),
+    path('schedule', views.schedule, name="schedule"),
 ]
