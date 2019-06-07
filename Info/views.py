c7165559e327919e202763f3f0bac7bc6f0b59a3
Barracoder <emmnauelobarany@gmail.com>
2019-06-07 20:35:20 +0300

    Added new routing functions.
    Signed-off-by: Barracoder <emmnauelobarany@gmail.com>

--- Info/views.py
+++ Info/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html", {"user": request.user})
+
+
+def about(request):
+    return render(request, "about.html", {"user": request.user})
+
+
+def faq(request):
+    return render(request, "faq.html", {"user": request.user})
+
+
+def schedule(request):
+    return render(request, "schedule.html", {"user": request.user})
