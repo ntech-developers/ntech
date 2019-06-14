887d3fcd7ac5b86d44a4b68c7d8220b35bc7f737
Barracoder <emmnauelobarany@gmail.com>
2019-06-14 15:19:03 +0300

    Improved static files handling.
    Signed-off-by: Barracoder <emmnauelobarany@gmail.com>

--- ntech/settings.py
+++ ntech/settings.py
@@ -118,12 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # User uploaded files such as profile images
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # This prevents an error caused by image upload through ajax
 
